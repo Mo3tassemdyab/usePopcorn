ba9609a41ae205ab1333fb81e8479c2057599961
Mo3tassemdyab <moatassemdiab@gmail.com>
2024-12-07 16:57:22 +0200

    sec commit

--- src/App.js
+++ src/App.js
@@ -184,7 +184,7 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
     async function getMovieDetails() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=89df13eb&i=${selectedID}`
+        `https://www.omdbapi.com/?apikey=89df13eb&i=${selectedID}`
       );
       const data = await res.json();
       setMovie(data);
